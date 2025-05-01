8db44979cb210c23792d528095fc2bb5a45d5392
Caue Silva <86086370+cauesilva1@users.noreply.github.com>
2025-05-01 09:07:40 -0400

    style: adicionado um zoom nas imagens do menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
 
           <div className="flex flex-col gap-8 ">
             <p className="font-medium text-[25px]">
-              We’re passionate about turning simple moments into something
+              We're passionate about turning simple moments into something
               unforgettable — and we do it with Palha Italiana!
             </p>
             <p className="font-medium text-[25px]">
@@ -65,8 +65,8 @@ export default function Home() {
               perfect bite of happiness.
             </p>
             <p className="font-medium text-[25px]">
-              Whether it’s a treat for yourself or a gift for someone special,
-              we’re here to make life a little sweeter, one piece at a time.
+              Whether it's a treat for yourself or a gift for someone special,
+              we're here to make life a little sweeter, one piece at a time.
             </p>
           </div>
         </div>
@@ -97,17 +97,23 @@ export default function Home() {
               src={menu1}
               width={550}
               height={8}
-              alt="Palha Italiana" />
+              alt="Palha Italiana"
+              className="transition-transform duration-200 hover:scale-110 rounded-sm"
+            />
             <Image
               src={menu2}
               width={550}
               height={8}
-              alt="Palha Italiana" />
+              alt="Palha Italiana"
+              className="transition-transform duration-200 hover:scale-110 rounded-sm"
+            />
             <Image
               src={menu3}
               width={550}
               height={8}
-              alt="Palha Italiana" />
+              alt="Palha Italiana"
+              className="transition-transform duration-200 hover:scale-110 rounded-sm"
+            />
           </div>
           <p className="text-[0.98rem]">The combination of Brigadeiro (the real Brazilian cholocate fudge) combined with tea biscuits that gives the crispy sensation, covered by icing sugar, a holy bite!</p>
         </div>
